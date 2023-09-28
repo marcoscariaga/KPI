0306d1337442197d8db649565bb1eeb03ecb4782
marcoscariaga <marcoscariaga@hotmail.com>
2023-09-28 12:12:47 -0300

    Added spinner functionality to the pages

--- KPI/wwwroot/js/site.js
+++ KPI/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transionend", () => {
+        document.body.removeChild("loader");
+    })
+})
