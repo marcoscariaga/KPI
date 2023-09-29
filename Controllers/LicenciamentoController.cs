3edd1dfa2d2ec992129e23582e0aae6fce2776f7
marcos.cariaga <marcos.cariaga@rio.rj.gov.br>
2023-09-29 14:26:07 -0300

    Added bootstrap menu to the application. Must change to the right page.

--- Controllers/LicenciamentoController.cs
+++ Controllers/LicenciamentoController.cs
@@ -14,7 +14,14 @@ namespace KPI.Controllers
 
         public IActionResult Index()
         {
-            var totalLicenciados = _licenciamentoRepository.GetTotalLicencasDeferidas();
+            var totalLicenciados = _licenciamentoRepository.GetTotalLicenciados(2023,new List<int>(225053),5,true);
+
+            return View();
+        }
+
+        public IActionResult LicencasDeferidas()
+        {
+            var licencasDeferidas = _licenciamentoRepository.GetTotalLicencasDeferidas();
 
             return View();
         }
