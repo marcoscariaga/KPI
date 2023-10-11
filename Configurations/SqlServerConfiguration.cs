2420512e3cf1dca1bddd61459719116c4ed6706b
marcos.cariaga <marcos.cariaga@rio.rj.gov.br>
2023-10-11 16:56:56 -0300

    Adjustment on tooltips

--- Configurations/SqlServerConfiguration.cs
+++ Configurations/SqlServerConfiguration.cs
@@ -4,7 +4,11 @@
     {
         public static string GetConnectionString()
         {
-            return @"Data Source=clustersql2.rio.rj.gov.br;Initial Catalog=SISVISA;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            //Production
+            //return @"Data Source=clustersql2.rio.rj.gov.br;Initial Catalog=SISVISA;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            //Stagging
+            return @"Data Source=srv000107\hom;Initial Catalog=SISVISA;User ID=user_sisvisa;Password=va0wek0HieGaoJu;Integrated Security=False;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         }
     }
 }
